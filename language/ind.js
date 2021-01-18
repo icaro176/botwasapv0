aaabc3e8e6e94fbd46b24f5da6d7ea864e473b72
icaro176 <76911826+icaro176@users.noreply.github.com>
2021-01-18 10:49:08 -0300

    ind.js

--- language/ind.js
+++ language/ind.js
@@ -75,7 +75,7 @@ exports.clears = () => {
 }
 
 exports.pc = () => {
-	return`*「 REGISTRO 」 *\n\npara saber se você se registrou, verifique a mensagem que enviei \n\nNOTA:\n*se você não recebeu uma mensagem.  significa que você não salvou o número do bot*`
+	return`*「 REGISTRO 」*\n\npara saber se você se registrou, verifique a mensagem que enviei \n\nNOTA:\n*se você não recebeu uma mensagem.  significa que você não salvou o número do bot*`
 }
 
 exports.registered = (namaUser, umurUser, serialUser, time, sender) => {
@@ -95,37 +95,37 @@ exports.reglevelaha = (command, pushname, getLevelingLevel, sender, aha) => {
 }
 
 exports.reglevelahb = (command, pushname, getLevelingLevel, sender, ahb) => {
-	return`*Desculpa ${pushname} seu nível não é suficiente*\n\n*┏⊱seu nível : ${getLevelingLevel(sender)}*\n*┣tipo de comando de comando : ${command}*\n* nível condicional : ${ahb}*\n\n_NOTA : CHAT/SEMPRE LIGADO PARA OBTER XP_`
+	return`*Desculpa ${pushname} seu nível não é suficiente*\n\n*┏⊱seu nível : ${getLevelingLevel(sender)}*\n*┣⊱tipo de comando de comando : ${command}*\n* ┣nível condicional : ${ahb}*\n\n_NOTA : CHAT/SEMPRE LIGADO PARA OBTER XP_`
 }
 
 exports.reglevelahc = (command, pushname, getLevelingLevel, sender, ahc) => {
-	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahc}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+	return`*Desculpe, ${pushname} seu nível não é suficiente*\n\n*┏⊱level : ${getLevelingLevel(sender)}*\n* ┣⊱tipo de comando : ${command}*\n* ┣nível de nível: ${ahc}*\n\n_NOTA: CHAT/SEMPRE LIGADO PARA OBTER XP_`
 }
 
 exports.reglevelahd = (command, pushname, getLevelingLevel, sender, ahd) => {
-	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahd}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+	return`*Desculpa ${pushname} seu nível não é suficiente*\n\n*┏⊱seu nivel : ${getLevelingLevel(sender)}*\n*┣⊱tipo de comando : ${command}*\n*┗⊱requisitos de nível : ${ahd}*\n\n_NOTA : CHAT/SEMPRE LIGADO PARA OBTER XP_`
 }
 
 exports.reglevelahe = (command, pushname, getLevelingLevel, sender, ahe) => {
-	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahe}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+	return`*Desculpa ${pushname} seu nível não é suficiente*\n\n*┏⊱seu nivel : ${getLevelingLevel(sender)}*\n*┣⊱tipo de comando : ${command}*\n*┗⊱requisitos de nível : ${ahe}*\n\n_NOTA : CHAT/SEMPRE LIGADO PARA OBTER XP_`
 }
 
 exports.reglevelahf = (command, pushname, getLevelingLevel, sender, ahf) => {
-	return`*Maaf ${pushname} level mu belum mencukupi*\n\n*┏⊱level mu : ${getLevelingLevel(sender)}*\n*┣⊱jenis command : ${command}*\n*┗⊱syarat level : ${ahf}*\n\n_NOTE : CHAT/SELALU ON UNTUK MENDAPATKAN XP_`
+	return`*Desculpa ${pushname} seu nível não é suficiente*\n\n*┏⊱seu nível : ${getLevelingLevel(sender)}*\n*┣⊱tipo de comando : ${command}*\n*┗⊱requisitos de nível : ${ahf}*\n\n_NOTA : CHAT/SEMPRE LIGADO PARA OBTER XP_`
 }
 
 exports.antilinker = (pushname) => {
-	return`*Maaf ${pushname}*\n*anda di kick dari group karena telah mengirim link group lain*`
+	return`*Desculpa ${pushname}*\n*Você foi expulso do grupo por enviar outro link de grupo*`
 }
 
 exports.detectorOnAlready = () => {
-	return`*SEBELUM NYA ANTILINK SUDAH AKTIF*`
+	return`*ANTES DE SEU ANTILINK SER ATIVADO*`
 }
 
 exports.antilinkOn = (groupName) => {
-	return`*「 ANTILINK 」*\n\n*perhatian group ${groupName} telah mengaktifkan fitur antilink*\n*jika kalian mengirimkan link group maka akan di kick otomatis*`
+	return`*「 ANTILINK 」*\n\n*atenção do grupo ${groupName} ativou o recurso antilink*\n*se você enviar um link de grupo, você será banido automaticamente*`
 }
 
 exports.antilinkOf = () => {
-	return`*「 ANTILINK 」NON AKTIF*`
+	return`*「 ANTILINK 」NÃO ATIVO*`
 }
